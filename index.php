179ed8e17c4e9dcfe0a2c611413d1290dd68c262
Md. Ariful Hossain <ariful15-12473@diu.edu.bd>
2022-04-10 23:53:49 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bornomala Library</title>
+    <link rel="icon" href="Images/blogo.png">
+    <link rel="stylesheet" href="first.css">
+</head>
+<body>
+    <div class="easy">
+        <div class="navbar">
+            <img src="Images/blogo.png" alt="This is our Logo" class="logo">
+            <a href="second.php"><button type="button">Sign Up</button></a>
+        </div>
+        <div class="content">
+            <small>Welcome to our</small>
+            <h1>Bornomala Library</h1>
+            <h3>Dive Into A Sea Of Knowledge</h3>
+            <a href="second.php"><button type="button">Take a Tour</button></a>
+        </div>
+        <div class="side-bar">
+            <img src="Images/menu.png" alt="" class="menu">
+
+            <div class="social-links">
+                <img src="Images/fb.png" alt="">
+                <img src="Images/ig.png" alt="">
+                <img src="Images/tw.png" alt="">
+            </div>
+
+            <div class="useful-links">
+                <img src="Images/share.png" alt="">
+                <img src="Images/info.png" alt="">
+            </div>
+        </div>
+        <div class="bubbles">
+            <img src="Images/bubble.png" alt="">
+            <img src="Images/bubble.png" alt="">
+            <img src="Images/bubble.png" alt="">
+            <img src="Images/bubble.png" alt="">
+            <img src="Images/bubble.png" alt="">
+            <img src="Images/bubble.png" alt="">
+            <img src="Images/bubble.png" alt="">
+        </div>
+    </div>
+</body>
+</html>
