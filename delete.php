805ce8455b384e2ec7008efa7f4d6bb3d9d85700
Md Ariful Hossain <ariful15-12473@diu.edu.bd>
2022-05-27 00:04:56 +0600

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,10 @@
+<?php
+require_once '../dbcon.php';
+
+if(isset($_GET['bookdelete'])){
+    $id = base64_decode($_GET['bookdelete']);
+
+    mysqli_query($con,"DELETE FROM `books` WHERE `id`= '$id'");
+    header('location: manage-book.php');
+}
+?>
