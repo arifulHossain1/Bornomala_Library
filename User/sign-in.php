d809825c467aec6f00615bb3f1e8a33e925ad10c
Md Ariful Hossain <ariful15-12473@diu.edu.bd>
2022-06-02 22:55:18 +0600

    Add files via upload

---
+++ User/sign-in.php
@@ -0,0 +1,126 @@
+<?php
+require_once '../dbcon.php';
+
+session_start();
+if(isset($_SESSION['user_login'])){
+    header('location: index.php');
+}
+
+if(isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $result = mysqli_query($con, "SELECT * FROM `user` WHERE `email` = '$email' OR `username` = '$email'");
+    if(mysqli_num_rows($result) == 1){
+        $row = mysqli_fetch_assoc($result);
+        if(password_verify($password, $row['password'])){
+            if($row['status'] == 1){
+                $_SESSION['user_login'] = $email;
+                $_SESSION['user_id'] = $row['id'];
+                header('location: index.php');
+            }else{
+                $error = "Status inactive. Please contact with Library Admin!";
+            }
+        }else{
+            $error = "Invalid password!";
+        }
+    }else{
+        $error = "Invalid Email or Username!";
+    }
+    
+}
+?>
+<!doctype html>
+<html lang="en" class="fixed accounts sign-in">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <title>User Login</title>
+    <!--BASIC css-->
+    <!-- ========================================================= -->
+    <link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/vendor/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="../assets/vendor/animate.css/animate.css">
+    <!--SECTION css-->
+    <!-- ========================================================= -->
+    <!--TEMPLATE css-->
+    <!-- ========================================================= -->
+    <link rel="stylesheet" href="../assets/stylesheets/css/style.css">
+    <link rel="icon" href="../Images/blogo.png">
+</head>
+
+<body>
+<div class="wrap">
+    <!-- page BODY -->
+    <!-- ========================================================= -->
+    <div class="page-body animated slideInDown">
+        <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
+        <!--LOGO-->
+        <div class="logo">
+            <!--<img alt="logo" src="images/logo-dark.png" />-->
+            <h1 class="text-center">Bornomala Library</h1>
+            <?php
+                if(isset($error)){
+                    ?>
+                <div class="alert alert-danger" role="alert">
+                    <?= $error ?>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <?php
+                }
+                ?>
+        </div>
+        <div class="box">
+            <!--SIGN IN FORM-->
+            <div class="panel mb-none">
+                <div class="panel-content bg-scale-0">
+                    <form method="post" action="<?= $_SERVER['PHP_SELF'] ?>">
+                        <div class="form-group mt-md">
+                            <span class="input-with-icon">
+                                <input type="text" class="form-control" id="email" placeholder="Email or Username" name="email" value="<?= isset($email) ? $email:''?>">
+                                <i class="fa fa-envelope"></i>
+                            </span>
+                        </div>
+                        <div class="form-group">
+                            <span class="input-with-icon">
+                                <input type="password" class="form-control" id="password" placeholder="Password" name="password">
+                                <i class="fa fa-key"></i>
+                            </span>
+                        </div>
+                        <div class="form-group">
+                            <div class="checkbox-custom checkbox-primary">
+                                <input type="checkbox" id="remember-me" value="option1" checked>
+                                <label class="check" for="remember-me">Remember me</label>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-primary btn-block" value="Sign in" name="login">
+                        </div>
+                        <div class="form-group text-center">
+                            <a href="pages_forgot-password.html">Forgot password?</a>
+                            <hr/>
+                             <span>Don't have an account?</span>
+                            <a href="register.php" class="btn btn-block mt-sm">Register</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
+    </div>
+</div>
+<!--BASIC scripts-->
+<!-- ========================================================= -->
+<script src="../assets/vendor/jquery/jquery-1.12.3.min.js"></script>
+<script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="../assets/vendor/nano-scroller/nano-scroller.js"></script>
+<!--TEMPLATE scripts-->
+<!-- ========================================================= -->
+<script src="../assets/javascripts/template-script.min.js"></script>
+<script src="../assets/javascripts/template-init.min.js"></script>
+<!-- SECTION script and examples-->
+<!-- ========================================================= -->
+</body>
+</html>
