f0ed2a4f3eb3f4dcf255c12c2dbda7a3a152c57d
Md Ariful Hossain <ariful15-12473@diu.edu.bd>
2022-06-13 23:30:15 +0600

    Add files via upload

---
+++ Admin/status_active.php
@@ -0,0 +1,7 @@
+<?php
+require_once '../dbcon.php';
+$id = base64_decode($_GET['id']);
+
+mysqli_query($con, "UPDATE `user` SET `status` = '1' WHERE `id` = '$id'");
+header('location: users.php');
+?>
